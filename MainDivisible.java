9e253d94ecfe46099ebdcadf8a752d8c68a45096
Waldago <54184873+Waldago@users.noreply.github.com>
2019-08-19 18:35:30 -0300

    Create MainDivisible.java
    FizzBuzz: Imprimir por pantalla los números del 1 al 100 pero considerando lo
    siguiente:
    
    Si el número es divisible por 3 se debe imprimir “Fizz”.
    
    Si el número es divisible por 5 se debe imprimir “Buzz”.
    
    Si el número es divisible por 3 y por 5 se debe imprimir “FizzBuzz”.

---
+++ MainDivisible.java
@@ -0,0 +1,11 @@
+package ejercicioClase2a;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Divisible a = new Divisible();
+		a.getDivisible();
+	}
+
+}
